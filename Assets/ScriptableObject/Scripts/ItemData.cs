dc24e76c288c6ba5e1c8003bf517edc45d903da5
phw97123 <phw97123@naver.com>
2023-09-23 22:01:23 +0900

    [FEAT] 아이템 장착과 모션, 자원채취

--- Assets/ScriptableObject/Scripts/ItemData.cs
+++ Assets/ScriptableObject/Scripts/ItemData.cs
@@ -40,4 +40,8 @@ public class ItemData : ScriptableObject
     [Header("Consumable")]
     public ItemDataConsumable[] consumables;
 
+    [Header("Equip")]
+    public GameObject equipPrefab; 
+
+
 }
